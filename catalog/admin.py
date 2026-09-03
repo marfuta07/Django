378e1f4aa9dd5bfb9a2f547700b8029b7576a98b
Юлия Бусыгина <ubusygina747@gmail.com>
2026-09-03 18:27:07 +0500

    Внесены изменения

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from catalog.models import Product, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+    list_display_links = ('id', 'name')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
