00d3517dc33abe22fe5eac8706ae5e096cf7c933
Юлия Бусыгина <ubusygina747@gmail.com>
2026-09-03 14:22:06 +0500

    Внесены изменения

--- catalog/views.py
+++ catalog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
-from django.contrib import messages
-
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'catalog/home.html')
-
 
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-
-        if name and phone and message:
-            # Обработка данных
-            print(f'Сообщение от {name} ({phone}): {message}')
-
-            # Добавляем успешное сообщение
-            messages.success(request, 'Ваше сообщение успешно отправлено! Спасибо! ✅')
-        else:
-            # Добавляем сообщение об ошибке
-            messages.error(request, 'Пожалуйста, заполните все поля! ⚠️')
-
+        print(f'Сообщение от {name} ({phone}): {message}')
     return render(request, 'catalog/contacts.html')
